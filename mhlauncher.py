0dac39beb289c33993f35cb93e2e02c73faeca59
_MhwsChina_ <142726642+MhwsChina@users.noreply.github.com>
2025-04-30 19:13:19 +0800

    v0.0.41
    v0.0.41

--- mhlauncher.py
+++ mhlauncher.py
@@ -49,7 +49,7 @@ def walk(root,path=''):
         if os.path.isdir(pj(root,path,i)):paths=paths+walk(root,pj(path,i))
         paths.append((root,path,i))
     return paths
-version,s3='v0.0.40',getrizhi()
+version,s3='v0.0.41',getrizhi()
 print('正在加载配置文件...')
 opt=init()
 print('完成!')
@@ -61,7 +61,7 @@ if opt['check_update']:
     print('正在检查更新')
     try:
         th.Thread(target=check_update,args=(version,'mhl')).start()
-    except:print('失败')
+    except:raise;print('失败')
 class main_ui:
     def __init__(self):
         self.tmpa,self.tmpb,self.tmpc=0,0,0
