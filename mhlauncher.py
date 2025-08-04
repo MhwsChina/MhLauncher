59ffd9f32708090610c4239eb501621b115c0b18
_MhwsChina_ <142726642+MhwsChina@users.noreply.github.com>
2025-08-04 09:04:12 +0800

    v0.0.64
    v0.0.64

--- mhlauncher.py
+++ mhlauncher.py
@@ -55,7 +55,7 @@ def walk(root,path=''):
         if os.path.isdir(pj(root,path,i)):paths=paths+walk(root,pj(path,i))
         paths.append((root,path,i))
     return paths
-version,s3='v0.0.63',getrizhi()
+version,s3='v0.0.64',getrizhi()
 log('正在加载配置文件...')
 opt=init()
 log('完成!')
